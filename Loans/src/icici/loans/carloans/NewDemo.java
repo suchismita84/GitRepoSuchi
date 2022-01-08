67af3be52224c8b4456c3210324bd92b3d6dba74
suchismita84 <97323292+suchismita84@users.noreply.github.com>
2022-01-08 09:13:34 +0530

    new message added

--- Loans/src/icici/loans/carloans/NewDemo.java
+++ Loans/src/icici/loans/carloans/NewDemo.java
@@ -4,7 +4,8 @@ public class NewDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Newly added on 8th Jan");
-
+		
+		System.out.println("Newly added on 8th Jan, 2nd message");
 	}
 
 }
