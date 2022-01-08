080dcc99bdfb42616b7d6114d61dad23922a7bba
suchismita84 <97323292+suchismita84@users.noreply.github.com>
2022-01-08 09:21:23 +0530

    added new line in demo

--- Loans/src/icici/loans/carloans/NewDemo.java
+++ Loans/src/icici/loans/carloans/NewDemo.java
@@ -6,6 +6,7 @@ public class NewDemo {
 		System.out.println("Newly added on 8th Jan");
 		
 		System.out.println("Newly added on 8th Jan, 2nd message12");
+		System.out.println("Hello Good morning");
 	}
 
 }
